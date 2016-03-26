02ae2cd2b0b4f6857bd9a4e239ed2b5e826d5ef5
Benno Zeeman <bzeeman@live.nl>
2016-03-26 15:11:27 +0100

    Added PWM beta; bit of tweaking

--- main.c
+++ main.c
@@ -4,10 +4,13 @@
 #include <stdint.h>
 
 
+// Set pin from port to input or output direction
 #define GPIO_IN(port, pin)   (port->DIR  &= ~(1 << pin))
 #define GPIO_OUT(port, pin)  (port->DIR  |=  (1 << pin))
+// Set pin from port to low or high logical state
 #define GPIO_LO(port, pin)   (port->DATA &= ~(1 << pin))
 #define GPIO_HI(port, pin)   (port->DATA |=  (1 << pin))
+#define GPIO_GET(port, pin)  (port->DATA & (1 << pin))
 
 
 #define IRC_FREQ          12000000UL
@@ -23,6 +26,7 @@ static void sys_init(void)
     LPC_SYSCON->SYSAHBCLKDIV   = SYSAHBCLKDIV_int;
     LPC_SYSCON->SYSAHBCLKCTRL |= (1 << 16);
 }
+
 
 // Assumes values have reset values
 void uart_init()
@@ -56,7 +60,6 @@ void uart_init()
     // Disable access to Divisor Latches (thus allowing to put chars in THR)
     LPC_UART->LCR &= ~(1 << 7);
 }
-
 void uart_putc(const char c)
 {
     while(!(LPC_UART->LSR & (1 << 6)))
@@ -64,14 +67,12 @@ void uart_putc(const char c)
 
     LPC_UART->THR = c;
 }
-
 void uart_puts(const char *str)
 {
     while (*str != '\0') {
         uart_putc(*str++);
     }
 }
-
 void uart_putu(uint32_t i)
 {
     // Approximation of chars needed to fit int digits
@@ -94,29 +95,7 @@ void uart_puti(int32_t i)
     uart_putu(i);
 }
 
-void timer_init()
-{
-    // Enable clock for 32-bit timer
-    LPC_SYSCON->SYSAHBCLKCTRL |= (1 << 9);
-
-    // Enable the counter
-    LPC_TMR32B0->TCR |= (1 << 0);
-
-    // Maximum value for Prescale Counter ('divisor', 12 MHz / 12 = 1 MHz = tick per microsecond)
-    LPC_TMR32B0->PR = 11;
-}
-
-uint32_t timer_count(void)
-{
-    return LPC_TMR32B0->TC;
-}
-
-void wait(uint32_t usec)
-{
-    const uint32_t post = timer_count() + usec;
-    while (timer_count() < post);
-}
-
+// Todo: specify port and pin
 void adc_initialize(void) {
     LPC_IOCON->R_PIO1_0       &= ~0x87;        // reset FUNC (first 3 bits) and ADMODE to 0 (analog input mode)
     LPC_IOCON->R_PIO1_0       |= (1 << 1);     // set pin as analog input (AD1)
@@ -135,34 +114,98 @@ uint32_t adc_get(void) {
     return ((LPC_ADC->GDR & 0xFFC0) >> 6);
 }
 
+
+void timer_init()
+{
+    // Enable TMR32B0
+    LPC_SYSCON->SYSAHBCLKCTRL |= (1 << 9);
+
+    // Enable the counter
+    LPC_TMR32B0->TCR |= (1 << 0);
+
+    // TC increments every PR+1 (12 MHz / 12 = 1 MHz = 1 tick per microsecond)
+    LPC_TMR32B0->PR = 12 - 1;
+}
+uint32_t timer_count(void)
+{
+    return LPC_TMR32B0->TC;
+}
+void wait(uint32_t usec)
+{
+    const uint32_t post = timer_count() + usec;
+    while (timer_count() < post);
+}
+
+
+void pwm_init(void)
+{
+    // Set pin 1.1 to function CT32B1_MAT0
+    LPC_IOCON->R_PIO1_1 &= ~0x7;
+    LPC_IOCON->R_PIO1_1 |=  0x3;
+
+
+    // Enable TMR32B1
+    LPC_SYSCON->SYSAHBCLKCTRL |= (1 << 10);
+
+    // TC increments every PR+1 (12 MHz / 12 = 1 MHz = 1 tick per microsecond)
+    LPC_TMR32B1->PR = 256 - 1;
+
+    LPC_TMR32B1->MR0 = 30000;     // TC match
+    LPC_TMR32B1->MR3 = 65536 - 1; // Period
+
+    // Enable PWM modes for MAT0
+    LPC_TMR32B1->PWMC |= (1 << 0);
+
+    // Reset counter when MR3 is matched
+    LPC_TMR32B1->MCR |= (1 << 10);
+
+    // Enable counter
+    LPC_TMR32B1->TCR |= (1 << 0);
+}
+
+
 int main(void)
 {
     sys_init();
     uart_init();
-    timer_init();
-    adc_initialize();
+    //timer_init();
+    //adc_initialize();
+    pwm_init();
 
     uart_puts("Hello, World.\n");
+
+    GPIO_LO(LPC_GPIO1, 1);
+    uart_puti(GPIO_GET(LPC_GPIO1, 1));
+    uart_putc('\n');
+
+    while (1) {
+        uart_puti(LPC_TMR32B1->TC);
+        uart_putc('-');
+        uart_puti(GPIO_GET(LPC_GPIO1, 1));
+        uart_putc('\n');
+    }
 
     /*while (1) {
         uart_putu(adc_get());
         uart_putc('\n');
     }*/
 
-    /*GPIO_OUT(LPC_GPIO1, 9); // Configure as output
+    /* //Software PWM
 
-    uint32_t period1 = 2300;
-    uint32_t period2 = 20000 - period1;
-    uint32_t post    = timer_count();
+    GPIO_OUT(LPC_GPIO1, 8); // Configure as output
 
+    const uint32_t period1 = 2300,
+                   period2 = 20000 - period1;
+
+    uint32_t post = timer_count();
 
 	while (1) {
-        GPIO_HI(LPC_GPIO1, 9);
+        GPIO_HI(LPC_GPIO1, 8);
 
         post += period1;
         while (timer_count() < post);
 
-        GPIO_LO(LPC_GPIO1, 9);
+        GPIO_LO(LPC_GPIO1, 8);
 
         post += period2;
         while (timer_count() < post);
