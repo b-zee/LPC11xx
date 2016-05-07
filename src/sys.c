80faf75c7fc6da82521e68e3552a1a73f0718ad2
Benno Zeeman <bzeeman@live.nl>
2016-05-07 20:26:34 +0200

    Added function to retreive device ID

--- src/sys.c
+++ src/sys.c
@@ -8,3 +8,8 @@ void sys_init(void)
     LPC_SYSCON->SYSAHBCLKDIV   = SYSAHBCLKDIV_int;
     LPC_SYSCON->SYSAHBCLKCTRL |= (1 << 16);
 }
+
+uint32_t sys_device_id(void)
+{
+    return LPC_SYSCON->DEVICE_ID;
+}
--- src/sys.h
+++ src/sys.h
@@ -1,6 +1,9 @@
 #ifndef SYS_H
 #define SYS_H
 
-void sys_init(void);
+#include <stdint.h>
+
+void     sys_init(void);
+uint32_t sys_device_id(void)
 
 #endif
